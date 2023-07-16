205088e22a93800458155d0abc779c3ba7bc4bbb
bugsssssss <n.soatov@bk.ru>
2023-07-16 14:54:06 +0500

    insecure request fix

--- js/script.js
+++ js/script.js
@@ -1,34 +1,36 @@
 VANTA.WAVES({
-    el: ".your-element-selector",
-    color: 0x40404,
-    waveHeight: 4.50,
-    waveSpeed: 0.65
-})
+	el: ".your-element-selector",
+	color: 0x40404,
+	waveHeight: 4.5,
+	waveSpeed: 0.65,
+});
 
-const windowsBox = document.querySelectorAll('.contentBlock .box')
-const authorizationWindow = document.getElementById('authorizationDiv')
-const registrationWindow = document.getElementById('registrationDiv')
-const userWindow = document.getElementById('userPersonDiv')
-const friendsWindow = document.getElementById('friendsBox')
-const chatWindow = document.getElementById('chatBox')
-const inputChat = document.getElementById('inputChat')
-const userForChat = document.getElementById('anotherUserForChat')
+const windowsBox = document.querySelectorAll(".contentBlock .box");
+const authorizationWindow = document.getElementById("authorizationDiv");
+const registrationWindow = document.getElementById("registrationDiv");
+const userWindow = document.getElementById("userPersonDiv");
+const friendsWindow = document.getElementById("friendsBox");
+const chatWindow = document.getElementById("chatBox");
+const inputChat = document.getElementById("inputChat");
+const userForChat = document.getElementById("anotherUserForChat");
 
-const registrationBtn = document.getElementById('registration')
-const getHome = document.querySelectorAll('.getUserProfile')
-const friendsBtn = document.getElementById('friendsListBtn')
-const friendsList = document.querySelectorAll('.friends .friend')
+const registrationBtn = document.getElementById("registration");
+const getHome = document.querySelectorAll(".getUserProfile");
+const friendsBtn = document.getElementById("friendsListBtn");
+const friendsList = document.querySelectorAll(".friends .friend");
 
-const userEmailAutorization = document.getElementById('userEmailAutorization')
-const userPasswordAutorization = document.getElementById('userPasswordAutorization')
+const userEmailAutorization = document.getElementById("userEmailAutorization");
+const userPasswordAutorization = document.getElementById(
+	"userPasswordAutorization"
+);
 
-const userFirstNameRegistration = document.getElementById('userFirstNameRegistration')
-const userEmailRegistration = document.getElementById('userEmailRegistration')
-const userPasswordRegistration = document.getElementById('userPasswordRegistration')
-
-
-
-
+const userFirstNameRegistration = document.getElementById(
+	"userFirstNameRegistration"
+);
+const userEmailRegistration = document.getElementById("userEmailRegistration");
+const userPasswordRegistration = document.getElementById(
+	"userPasswordRegistration"
+);
 
 // async function authorizationCheckUser() {
 //     username = document.getElementById("myinput").value
@@ -38,41 +40,35 @@ const userPasswordRegistration = document.getElementById('userPasswordRegistrati
 //     getUser(username)
 // }
 
-
-
-
-
-
-
-
-
-
 // litseners
 
-document.getElementById('authorizationClick').addEventListener('click', authorizationCheckUser)
-document.getElementById('registrationClick').addEventListener('click', registrationCheckUser)
+document
+	.getElementById("authorizationClick")
+	.addEventListener("click", authorizationCheckUser);
+document
+	.getElementById("registrationClick")
+	.addEventListener("click", registrationCheckUser);
 
 friendsBtn.onclick = function () {
-    userWindow.classList.add('none')
-    friendsWindow.classList.remove('none')
-}
+	userWindow.classList.add("none");
+	friendsWindow.classList.remove("none");
+};
 registrationBtn.onclick = function () {
-    authorizationWindow.classList.add('none')
-    registrationWindow.classList.remove('none')
-}
-getHome.forEach (btn => {
-    btn.addEventListener('click', openProfile)
-})
-friendsList.forEach (btn => {
-    btn.addEventListener('click', function () {
-        let elem = event.target
-        userForChat.innerHTML = elem.innerText
-        friendsWindow.classList.add('none')
-        chatWindow.classList.remove('none')
-    })
-})
+	authorizationWindow.classList.add("none");
+	registrationWindow.classList.remove("none");
+};
+getHome.forEach((btn) => {
+	btn.addEventListener("click", openProfile);
+});
+friendsList.forEach((btn) => {
+	btn.addEventListener("click", function () {
+		let elem = event.target;
+		userForChat.innerHTML = elem.innerText;
+		friendsWindow.classList.add("none");
+		chatWindow.classList.remove("none");
+	});
+});
 // inputChat.addEventListener('input', autoresize);
-
 
 // functions
 // function autoresize() {
@@ -81,86 +77,84 @@ friendsList.forEach (btn => {
 //     input.style.transition = "none";
 //   }
 function openProfile() {
-    for (i = 0; i < windowsBox.length; i++){
-        windowsBox[i].classList.add('none')
-    }
-    for (i = 0; i < friendsList.length; i++){
-        friendsList[i].classList.remove('none')
-    }
-    userWindow.classList.remove('none')
+	for (i = 0; i < windowsBox.length; i++) {
+		windowsBox[i].classList.add("none");
+	}
+	for (i = 0; i < friendsList.length; i++) {
+		friendsList[i].classList.remove("none");
+	}
+	userWindow.classList.remove("none");
 }
 async function sendRequest(url, method, data, e) {
-    e.preventDefault()
-    url = `http://nurbek.lol:8000/api/${url}`
-    
-    if(method == "POST") {
-        let response = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-    
-        response = await response.json()
-        return response
-    } else if(method == "GET") {
-        url = url+"?"+ new URLSearchParams(data)
-        let response = await fetch(url, {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
-        response = await response.json()
-        return response
-    }
+	e.preventDefault();
+	url = `http://nurbek.lol:8000/api/${url}`;
+
+	if (method == "POST") {
+		let response = await fetch(url, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+
+		response = await response.json();
+		return response;
+	} else if (method == "GET") {
+		url = url + "?" + new URLSearchParams(data);
+		let response = await fetch(url, {
+			method: "GET",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		});
+		response = await response.json();
+		return response;
+	}
 }
 function authorizationCheckUser() {
-    // if (userEmailAutorization.value != )
-    let userInfo = {
-        email: userEmailAutorization.value,
-        password: userPasswordAutorization.value
-    }
-    getUser(userInfo)
+	// if (userEmailAutorization.value != )
+	let userInfo = {
+		email: userEmailAutorization.value,
+		password: userPasswordAutorization.value,
+	};
+	getUser(userInfo);
 }
 function registrationCheckUser() {
-    let userInfo = {
-        "first_name": userFirstNameRegistration.value,
-        "email": userEmailRegistration.value,
-        "password": userPasswordRegistration.value
-    }
-    regUser(userInfo)
+	let userInfo = {
+		first_name: userFirstNameRegistration.value,
+		email: userEmailRegistration.value,
+		password: userPasswordRegistration.value,
+	};
+	regUser(userInfo);
 }
 async function regUser(userInfo) {
-    // setCookie('username', username)
-    // document.querySelector('.registration').classList.add('none')
-    // отправить запрос на регистрацию пользователя
-    await sendRequest('user', 'POST', {userInfo})
-    getUser(userInfo) //???
+	// setCookie('username', username)
+	// document.querySelector('.registration').classList.add('none')
+	// отправить запрос на регистрацию пользователя
+	await sendRequest("user", "POST", { userInfo });
+	getUser(userInfo); //???
 }
 
 async function getUser(username) {
-    console.log(response)
-    // let response = await sendRequest(`auth/?email=${username.email}&password=${username.password}`, 'GET')
-    let response = await sendRequest('auth', 'GET', username)
+	console.log(response);
+	// let response = await sendRequest(`auth/?email=${username.email}&password=${username.password}`, 'GET')
+	let response = await sendRequest("auth", "GET", username);
 
-    if (response) {
-        // нет ошибки
-        let userInfo = document.getElementById('userName')
-        userInfo.innerHTML = `${response.id}`
-        openProfile()
+	if (response) {
+		// нет ошибки
+		let userInfo = document.getElementById("userName");
+		userInfo.innerHTML = `${response.id}`;
+		openProfile();
 
-        // if (){
-        // }
-    } else {
-        // есть ошибка
-        alert('Такого пользователя не судествует!')
-        
-        
-    }
+		// if (){
+		// }
+	} else {
+		// есть ошибка
+		alert("Такого пользователя не судествует!");
+	}
 }
 
 // async function getUser(username) {
@@ -179,10 +173,6 @@ async function getUser(username) {
 //     } else {
 //         // есть ошибка
 //         alert('Такого пользователя не судествует!')
-        
-        
+
 //     }
 // }
-
-
-
